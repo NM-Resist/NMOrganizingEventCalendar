5d94ba0c1c3c8a1a02367d6059264b0c49d99d5c
Matthew Sumpter <40173937+mjsumpter@users.noreply.github.com>
2026-03-31 19:41:04 -0600

    Update contact email

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@
 	class="mt-6 rounded border border-yellow-200 bg-yellow-50 p-2 text-center text-sm text-yellow-800"
 >
 	Want to add an event to the calendar? Email us at
-	<a href="mailto:ta7375535@gmail.com" class="text-yellow-900 underline hover:text-yellow-700">
-		ta7375535@gmail.com
+	<a href="mailto:protestfornewmexico@gmail.com" class="text-yellow-900 underline hover:text-yellow-700">
+		protestfornewmexico@gmail.com
 	</a>
 </p>
